e4e268f221588577cffb1f4242180f0c6e84ca98
ManuelFBS <manuelf.borrego@gmail.com>
2023-12-27 00:05:36 -0400

    Actualización de app.rs

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,40 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let mut reviews: HashMap<String, String> = HashMap::new();
+
+    reviews.insert(
+        String::from("Ancient Roman History"),
+        String::from("Very accurate."),
+    );
+    reviews.insert(
+        String::from("Cooking with Rhubard"),
+        String::from("Sweet recipes."),
+    );
+    reviews.insert(
+        String::from("Cosmos"),
+        String::from("All about deep space."),
+    );
+    reviews.insert(
+        String::from("Programming in Rust"),
+        String::from("Great Examples."),
+    );
+    reviews.insert(
+        String::from("Black Holes & Time Warps: Einstein's Outrageous Legacy"),
+        String::from("Mistery Black Hole."),
+    );
+
+    // Se busca una revisión específica...
+    let book: &str = "Programming in Rust";
+    println!("\nReview for \'{}\': {:?}", book, reviews.get(book));
+
+    //////////////////////////////////////////////////////////
+
+    // Se elimina una 'review'...
+    let obsolete: &str = "Ancient Roman History";
+    println!("\n'{}\' removed.", obsolete);
+    reviews.remove(obsolete);
+
+    // Confirma que ha sido eliminada la 'review'...
+    println!("\nReview for \'{}\': {:?}", obsolete, reviews.get(obsolete));
 }
