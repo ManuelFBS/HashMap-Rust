e9bc7debdb493fd2925c917e6e0aa266bdf1b128
ManuelFBS <manuelf.borrego@gmail.com>
2024-01-02 14:16:29 -0400

    Actualizando exercise.rs

--- src/exercise.rs
+++ src/exercise.rs
@@ -55,27 +55,34 @@ fn car_factory(order: i32, miles: u32) -> Car {
     let mut roof = true;
     if order % 3 == 0 {
         // 3, 6, 9
-        motor = Transmission::Manual;
+        motor = Transmission::Automatic;
     } else if order % 2 == 0 {
         // 2, 4, 8, 10
         motor = Transmission::SemiAuto;
         roof = false;
     } else {
         // 1, 5, 7, 11
-        motor = Transmission::Automatic;
+        motor = motor;
         roof = true
     }
 
     // Devolver auto requerido...
     Car {
         color: String::from(colors[(color - 1) as usize]),
-        motor: motor,
-        roof: roof,
+        motor,
+        roof,
         age: car_quality(miles),
     }
 }
 
 fn main() {
+    // Se inicializa un 'hash map' para las órdenes
+    // de los autos...
+    // - Key: Car order number, i32...
+    // - Value: Car order details, Car struct...
+    use std::collections::HashMap;
+    let mut orders: HashMap<i32, Car> = HashMap::new();
+
     // Se inicializa variable contadora...
     let mut order = 1;
     // Se declara un car como mutable "Car" struct...
@@ -85,8 +92,60 @@ fn main() {
     // para cada requisición...
     // Orden para el auto #1: Usado, Techo rígido...
     car = car_factory(order, 1000);
-    println!(
-        "{}: {:?}, Hard top = {}, {:?}, {}, {} miles",
-        order, car.age.0, car.roof, car.motor, car.color, car.age.1
-    );
+    orders.insert(order, car);
+    // println!(
+    //     "{}: {:?}, Hard top = {}, {:?}, {}, {} miles",
+    //     order, car.age.0, car.roof, car.motor, car.color, car.age.1
+    // );
+    println!("Car order {}: {:?}", order, orders.get(&order));
+
+    // Orden para el auto #2: Usado, Convertible...
+    order = order + 1;
+    car = car_factory(order, 2000);
+    orders.insert(order, car);
+    // println!(
+    //     "{}: {:?}, Hard top = {}, {:?}, {}, {} miles",
+    //     order, car.age.0, car.roof, car.motor, car.color, car.age.1
+    // );
+    println!("Car order {}: {:?}", order, orders.get(&order));
+
+    // Orden para el auto #3: Nuevo, Techo rígido...
+    order = order + 1;
+    car = car_factory(order, 0);
+    orders.insert(order, car);
+    // println!(
+    //     "{}: {:?}, Hard top = {}, {:?}, {}, {} miles",
+    //     order, car.age.0, car.roof, car.motor, car.color, car.age.1
+    // );
+    println!("Car order {}: {:?}", order, orders.get(&order));
+
+    // Orden para el auto #4: Nuevo, Convertible...
+    order = order + 1;
+    car = car_factory(order, 0);
+    orders.insert(order, car);
+    // println!(
+    //     "{}: {:?}, Hard top = {}, {:?}, {}, {} miles",
+    //     order, car.age.0, car.roof, car.motor, car.color, car.age.1
+    // );
+    println!("Car order {}: {:?}", order, orders.get(&order));
+
+    // Orden para el auto #5: Usado, Techo rígido...
+    order = order + 1;
+    car = car_factory(order, 3000);
+    orders.insert(order, car);
+    // println!(
+    //     "{}: {:?}, Hard top = {}, {:?}, {}, {} miles",
+    //     order, car.age.0, car.roof, car.motor, car.color, car.age.1
+    // );
+    println!("Car order {}: {:?}", order, orders.get(&order));
+
+    // Orden para el auto #6: Usado, Techo rígido
+    order = order + 1;
+    car = car_factory(order, 4000);
+    orders.insert(order, car);
+    // println!(
+    //     "{}: {:?}, Hard top = {}, {:?}, {}, {} miles",
+    //     order, car.age.0, car.roof, car.motor, car.color, car.age.1
+    // );
+    println!("Car order {}: {:?}", order, orders.get(&order));
 }
